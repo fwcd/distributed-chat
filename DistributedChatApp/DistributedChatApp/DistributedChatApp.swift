8b7c36135f72bdfcc89d50c42ae15540c93b9e7c
fwcd <fwcdmail@gmail.com>
2021-01-17 01:53:37 +0100

    Add iOS app stub

---
+++ DistributedChatApp/DistributedChatApp/DistributedChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistributedChatAppApp.swift
+//  DistributedChatApp
+//
+//  Created by Fredrik on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct DistributedChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
