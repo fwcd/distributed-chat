29c92f7077af16572a181c027d5770a5c02383b8
fwcd <fwcdmail@gmail.com>
2021-01-17 01:30:11 +0100

    Update package manifest

--- Package.swift
+++ Package.swift
@@ -4,12 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "DistributedChat",
+    name: "distributed-chat",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DistributedChat",
-            targets: ["DistributedChat"]),
+            targets: ["DistributedChat"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DistributedChat",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "DistributedChatTests",
-            dependencies: ["DistributedChat"]),
+            dependencies: ["DistributedChat"]
+        ),
     ]
 )
