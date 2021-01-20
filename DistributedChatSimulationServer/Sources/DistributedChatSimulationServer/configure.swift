b8697160c519c076c38917dd76d9400388528c2b
fwcd <fwcdmail@gmail.com>
2021-01-20 19:29:49 +0100

    Add stylesheet and update header of simulation server page

--- DistributedChatSimulationServer/Sources/DistributedChatSimulationServer/configure.swift
+++ DistributedChatSimulationServer/Sources/DistributedChatSimulationServer/configure.swift
@@ -3,12 +3,9 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.views.use(.leaf)
-
-    
 
     // register routes
     try routes(app)
--- DistributedChatSimulationServer/Sources/DistributedChatSimulationServer/routes.swift
+++ DistributedChatSimulationServer/Sources/DistributedChatSimulationServer/routes.swift
@@ -2,10 +2,12 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
+        return req.view.render("index", [
+            "title": "Distributed Chat Simulation Server"
+        ])
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
+    // app.get("hello") { req -> String in
+    //     return "Hello, world!"
+    // }
 }
