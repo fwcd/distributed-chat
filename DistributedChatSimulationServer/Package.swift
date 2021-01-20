94c8e5eed920e7544a151df43bcd405e1f6608ce
fwcd <fwcdmail@gmail.com>
2021-01-20 19:08:59 +0100

    Add dependency on Leaf

--- DistributedChatSimulationServer/Package.swift
+++ DistributedChatSimulationServer/Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(path: "../DistributedChat"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +26,8 @@ let package = Package(
         .target(
             name: "DistributedChatSimulationServer",
             dependencies: [
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Leaf", package: "leaf")
             ]
         ),
         .testTarget(
