a7879da013e78b7f2a26fde70295bdbadefa4f3c
fwcd <fwcdmail@gmail.com>
2021-01-20 20:52:51 +0100

    Parse protocol messages in simulation server

--- DistributedChatSimulationServer/Sources/DistributedChatSimulationServer/MessagingHandler.swift
+++ DistributedChatSimulationServer/Sources/DistributedChatSimulationServer/MessagingHandler.swift
@@ -2,6 +2,7 @@ import Foundation
 import Vapor
 
 fileprivate let log = Logger(label: "ClientManager")
+fileprivate let decoder = JSONDecoder()
 
 class MessagingHandler {
     private var clients: [UUID: WebSocket] = [:]
@@ -12,9 +13,13 @@ class MessagingHandler {
 
         log.info("Opened connection to \(uuid)")
 
-        ws.onText { _, text in
-            // Echo text
-            ws.send(text)
+        ws.onText { _, raw in
+            do {
+                let msg = try decoder.decode(MessagingProtocol.Message.self, from: raw.data(using: .utf8)!)
+                log.info("Got \(msg)")
+            } catch {
+                log.error("Invalid protocol message: \(raw) - \(error)")
+            }
         }
 
         ws.onClose.whenComplete { _ in
