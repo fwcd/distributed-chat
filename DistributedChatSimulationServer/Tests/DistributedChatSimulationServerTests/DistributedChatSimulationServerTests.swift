b85db0773ed4429446952ce49a33795716a18780
fwcd <fwcdmail@gmail.com>
2021-01-21 17:25:11 +0100

    Use setUp/tearDown in simulation server testcase

--- DistributedChatSimulationServer/Tests/DistributedChatSimulationServerTests/DistributedChatSimulationServerTests.swift
+++ DistributedChatSimulationServer/Tests/DistributedChatSimulationServerTests/DistributedChatSimulationServerTests.swift
@@ -5,12 +5,18 @@ final class DistributedChatSimulationServerTests: XCTestCase {
     static var allTests = [
         ("testWebFrontend", testWebFrontend)
     ]
+    var app: Application!
+
+    override func setUpWithError() throws {
+        app = Application(.testing)
+        try configure(app)
+    }
+
+    override func tearDown() {
+        app.shutdown()
+    }
 
     func testWebFrontend() throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try configure(app)
-
         try app.test(.GET, "/", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssert(res.body.string.contains("<h1>Distributed Chat Simulation Server</h1>"))
