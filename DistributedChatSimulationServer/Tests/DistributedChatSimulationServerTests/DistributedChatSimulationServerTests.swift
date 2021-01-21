b5740910815dc9ed1958b1220b9053529b5a8ca9
fwcd <fwcdmail@gmail.com>
2021-01-21 17:21:13 +0100

    Fix simulation server test

--- DistributedChatSimulationServer/Tests/DistributedChatSimulationServerTests/DistributedChatSimulationServerTests.swift
+++ DistributedChatSimulationServer/Tests/DistributedChatSimulationServerTests/DistributedChatSimulationServerTests.swift
@@ -3,17 +3,17 @@ import XCTVapor
 
 final class DistributedChatSimulationServerTests: XCTestCase {
     static var allTests = [
-        ("testHelloWorld", testHelloWorld)
+        ("testWebFrontend", testWebFrontend)
     ]
 
-    func testHelloWorld() throws {
+    func testWebFrontend() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
 
-        try app.test(.GET, "hello", afterResponse: { res in
+        try app.test(.GET, "/", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
+            XCTAssert(res.body.string.contains("<h1>Distributed Chat Simulation Server</h1>"))
         })
     }
 }
