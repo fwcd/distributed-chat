d9e64cb47faa1162f93cd3a9a72493d76873fb78
fwcd <fwcdmail@gmail.com>
2021-01-21 20:00:19 +0100

    Set up basic REPL for CLI using LineNoise

--- DistributedChatCLI/Sources/DistributedChatCLI/main.swift
+++ DistributedChatCLI/Sources/DistributedChatCLI/main.swift
@@ -1,3 +1,13 @@
 import LineNoise
 
-print("Hello, world!")
+let ln = LineNoise()
+
+while let input = try? ln.getLine(prompt: "> ") {
+    ln.addHistory(input)
+    print()
+
+    // TODO: Actually handle input
+    print(input)
+}
+
+print()
