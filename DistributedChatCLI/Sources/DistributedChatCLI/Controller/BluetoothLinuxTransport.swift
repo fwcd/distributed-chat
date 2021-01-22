e9a58c0f5dbc8b505ee297d18651fc9302797e7b
fwcd <fwcdmail@gmail.com>
2021-01-22 21:04:19 +0100

    Open L2CAP server with BluetoothLinux in CLI

--- DistributedChatCLI/Sources/DistributedChatCLI/Controller/BluetoothLinuxTransport.swift
+++ DistributedChatCLI/Sources/DistributedChatCLI/Controller/BluetoothLinuxTransport.swift
@@ -7,9 +7,14 @@ import BluetoothLinux
 fileprivate let log = Logger(label: "BluetoothLinuxTransport")
 
 public struct BluetoothLinuxTransport: ChatTransport {
+    private let l2CapServer: L2CAPSocket
+
     public init() throws {
         guard let hostController = BluetoothLinux.HostController.default else { throw BluetoothLinuxError.noHostController }
-        log.info("Found host controller \(hostController)")
+        log.info("Found host controller \(hostController.identifier) with address \(try! hostController.readDeviceAddress())")
+
+        l2CapServer = try L2CAPSocket.lowEnergyServer()
+        log.info("Opened L2CAP server with PSM \(l2CapServer.protocolServiceMultiplexer)")
     }
 
     public func broadcast(_ raw: String) {
