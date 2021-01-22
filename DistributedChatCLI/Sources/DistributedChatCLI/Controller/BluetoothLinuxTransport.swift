1a3773f562f1d12ea86e3fd8010e5888eb6aed4f
fwcd <fwcdmail@gmail.com>
2021-01-22 22:31:13 +0100

    Add BLE service/characteristic UUIDs to BluetoothLinuxTransport

--- DistributedChatCLI/Sources/DistributedChatCLI/Controller/BluetoothLinuxTransport.swift
+++ DistributedChatCLI/Sources/DistributedChatCLI/Controller/BluetoothLinuxTransport.swift
@@ -1,11 +1,19 @@
 #if os(Linux)
 import DistributedChat
+import Foundation
 import Logging
 import Bluetooth
 import BluetoothHCI
 import BluetoothLinux
 
 fileprivate let log = Logger(label: "BluetoothLinuxTransport")
+
+// TODO: Ideally move these constants into a module shared with the CoreBluetooth version
+
+/// Custom UUID specifically for the 'Distributed Chat' service
+fileprivate let serviceUUID = UUID(uuidString: "59553ceb-2ffa-4018-8a6c-453a5292044d")!
+/// Custom UUID specific to the characteristic holding the L2CAP channel's PSM (see below)
+fileprivate let characteristicUUID = UUID(uuidString: "440a594c-3cc2-494a-a08a-be8dd23549ff")!
 
 public struct BluetoothLinuxTransport: ChatTransport {
     private let l2CapServer: L2CAPSocket
