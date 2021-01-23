8b773f14e7beb4c5dbb1c55e501c7928d89b27a9
fwcd <fwcdmail@gmail.com>
2021-01-23 17:04:04 +0100

    Show author in message bubble

--- DistributedChatApp/DistributedChatApp/View/BubbleMessageView.swift
+++ DistributedChatApp/DistributedChatApp/View/BubbleMessageView.swift
@@ -14,26 +14,31 @@ struct BubbleMessageView: View {
     
     var body: some View {
         ZStack {
-            Text("Hello")
-                .foregroundColor(isMe ? .black : .white)
-                .padding(10)
-                .background(isMe
-                    ? LinearGradient(gradient: Gradient(colors: [
-                          Color(red: 0.9, green: 0.9, blue: 0.9),
-                          Color(red: 0.9, green: 0.9, blue: 0.9)
-                      ]), startPoint: .top, endPoint: .bottom)
-                    : LinearGradient(gradient: Gradient(colors: [
-                          Color(red: 0, green: 0.5, blue: 1),  // Blue
-                          Color(red: 0, green: 0.4, blue: 0.7) // Darker blue
-                      ]), startPoint: .top, endPoint: .bottom)
-                )
-                .cornerRadius(10)
+            VStack(alignment: .leading) {
+                Text(message.author.displayName)
+                    .font(.caption)
+                    .foregroundColor(isMe ? .gray : .white)
+                Text(message.content)
+            }
+            .foregroundColor(isMe ? .black : .white)
+            .padding(10)
+            .background(isMe
+                ? LinearGradient(gradient: Gradient(colors: [
+                        Color(red: 0.9, green: 0.9, blue: 0.9),
+                        Color(red: 0.9, green: 0.9, blue: 0.9)
+                    ]), startPoint: .top, endPoint: .bottom)
+                : LinearGradient(gradient: Gradient(colors: [
+                        Color(red: 0, green: 0.5, blue: 1),  // Blue
+                        Color(red: 0, green: 0.4, blue: 0.7) // Darker blue
+                    ]), startPoint: .top, endPoint: .bottom)
+            )
+            .cornerRadius(10)
         }
     }
 }
 
 struct BubbleMessageView_Previews: PreviewProvider {
-    static let message = ChatMessage(author: ChatUser(name: "Alice"), content: "Test")
+    static let message = ChatMessage(author: ChatUser(name: "Alice"), content: "This is a long\nmultiline message!")
     static var previews: some View {
         VStack {
             BubbleMessageView(message: message, isMe: false)
