d043a78d3846c32f7913054fe41a10f46c491d46
fwcd <fwcdmail@gmail.com>
2021-01-23 22:34:51 +0100

    Expose writable GATT characteristic instead of L2CAP channel

--- DistributedChatApp/DistributedChatApp/Controller/CoreBluetoothTransport.swift
+++ DistributedChatApp/DistributedChatApp/Controller/CoreBluetoothTransport.swift
@@ -15,14 +15,15 @@ fileprivate let log = Logger(label: "CoreBluetoothTransport")
 
 /// Custom UUID specifically for the 'Distributed Chat' service
 fileprivate let serviceUUID = CBUUID(string: "59553ceb-2ffa-4018-8a6c-453a5292044d")
-/// Custom UUID specific to the characteristic holding the L2CAP channel's PSM (see below)
+/// Custom UUID for the (write-only) message inbox characteristic
 fileprivate let characteristicUUID = CBUUID(string: "440a594c-3cc2-494a-a08a-be8dd23549ff")
 
 class CoreBluetoothTransport: NSObject, ChatTransport, CBPeripheralManagerDelegate, CBCentralManagerDelegate {
     private var peripheralManager: CBPeripheralManager!
     private var centralManager: CBCentralManager!
     
-    private var startedL2CAPPublish: Bool = false
+    private var initialized: Bool = false
+    private var listeners = [(String) -> Void]()
     
     private let settings: Settings
     private var settingsSubscription: AnyCancellable?
@@ -36,13 +37,9 @@ class CoreBluetoothTransport: NSObject, ChatTransport, CBPeripheralManagerDelega
         self.settings = settings
         super.init()
         
-        // The app acts both as a peripheral (for receiving messages and
-        // exposing an L2CAP channel) and a central (for sending messages
+        // The app acts both as a peripheral (for receiving messages via an
+        // exposed, writable GATT characteristic) and a central (for sending messages
         // and discovering nearby devices).
-        //
-        // The peripheral is responsible for opening an L2CAP channel. For this,
-        // CoreBluetooth assigns it a free PSM, which it then advertises
-        // using a GATT characteristic.
         
         peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         centralManager = CBCentralManager(delegate: self, queue: nil)
@@ -61,7 +58,7 @@ class CoreBluetoothTransport: NSObject, ChatTransport, CBPeripheralManagerDelega
     }
     
     func onReceive(_ handler: @escaping (String) -> Void) {
-        // TODO
+        listeners.append(handler)
     }
     
     // MARK: Peripheral implementation
@@ -71,9 +68,9 @@ class CoreBluetoothTransport: NSObject, ChatTransport, CBPeripheralManagerDelega
         case .poweredOn:
             log.info("Peripheral is powered on!")
             
-            if !startedL2CAPPublish {
-                startedL2CAPPublish = true
-                peripheral.publishL2CAPChannel(withEncryption: false)
+            if !initialized {
+                initialized = true
+                publishService()
             } else if settings.bluetoothAdvertisingEnabled {
                 startAdvertising()
             }
@@ -86,25 +83,19 @@ class CoreBluetoothTransport: NSObject, ChatTransport, CBPeripheralManagerDelega
         }
     }
     
-    func peripheralManager(_ peripheral: CBPeripheralManager, didPublishL2CAPChannel psm: CBL2CAPPSM, error: Error?) {
-        log.info("Published L2CAP channel with PSM \(psm)")
+    private func publishService() {
+        log.info("Publishing DistributedChat GATT service...")
         
-        // Now that CoreBluetooth has assigned us a PSM for the L2CAP channel,
-        // publish it through the GATT characteristic. For this, we use a
-        // big-endian representation.
-        
-        var psm = psm.bigEndian
         let service = CBMutableService(type: serviceUUID, primary: true)
         let characteristic = CBMutableCharacteristic(type: characteristicUUID,
-                                                     properties: [.read],
-                                                     value: Data(bytes: &psm, count: MemoryLayout.size(ofValue: psm)),
-                                                     permissions: [.readable])
+                                                     properties: [.writeWithoutResponse],
+                                                     value: nil,
+                                                     permissions: [.writeable])
         
         service.characteristics = [characteristic]
         peripheralManager.add(service)
         
         startAdvertising()
-        // TODO: unpublishL2CAPChannel e.g. through a UI switch for disabling connectivity
     }
     
     private func startAdvertising() {
@@ -115,17 +106,22 @@ class CoreBluetoothTransport: NSObject, ChatTransport, CBPeripheralManagerDelega
         ])
     }
     
-    func peripheralManager(_ peripheral: CBPeripheralManager, didUnpublishL2CAPChannel PSM: CBL2CAPPSM, error: Error?) {
-        stopAdvertising()
-    }
-    
     private func stopAdvertising() {
         log.info("Stopping advertisting")
         peripheralManager.stopAdvertising()
     }
     
-    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
-        log.info("Got a GATT read request")
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        for request in requests {
+            // TODO: Deal with offset?
+            if let data = request.value, let str = String(data: data, encoding: .utf8) {
+                log.info("Received write to inbox: '\(str)'")
+                
+                for listener in listeners {
+                    listener(str)
+                }
+            }
+        }
     }
     
     // MARK: Central implementation
