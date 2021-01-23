2393c97a0633ff70740f164554a2e728eedd820b
fwcd <fwcdmail@gmail.com>
2021-01-23 22:36:53 +0100

    Add prompt for writing string to characteristic

--- Scripts/discover_service.py
+++ Scripts/discover_service.py
@@ -1,14 +1,13 @@
 # This is a small script that acts as a GATT central/client
 # for discovering real nodes (i.e. devices running the iOS app).
 # 
-# Those should be advertising a GATT service with a PSM on which
-# they expose an L2CAP channel.
+# Once our DistributedChat service has been discovered, it
+# prompts for a string to write to our 'message inbox'
+# characteristic.
 #
 # To use, run 'pip3 install bluepy'
 
-# NOTE: This script MUST run as root! Also, it can currently
-#       only discover the PSM of a node, not open the L2CAP
-#       channel to it.
+# NOTE: This script MUST run as root!
 
 from bluepy.btle import Scanner, Peripheral
 import socket
@@ -25,20 +24,14 @@ while True:
         for (adtype, desc, value) in dev.getScanData():
             print(f'Adtype: {adtype}, desc: {desc}, value: {value}')
             if adtype == 7 and value == SERVICE_UUID:
-                print('  >> Found the DistributedChat service, reading characteristic...')
+                print('  >> Found the DistributedChat service, finding characteristics...')
                 peripheral = Peripheral(dev.addr, dev.addrType, dev.iface)
                 characteristics = peripheral.getCharacteristics(uuid=CHARACTERISTIC_UUID)
                 if characteristics:
-                    # The PSM is stored in big-endian form
-                    psm = int.from_bytes(characteristics[0].read(), 'big')
-                    print(f'  >> Read PSM {psm} from our DistributedChat-specific GATT characteristic')
-
-                    # TODO: Python sockets do not support Bluetooth LE, see
-                    #       https://bugs.python.org/issue25056
-
-                    # s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_SEQPACKET, socket.BTPROTO_L2CAP)
-                    # print(f'  >> Connecting to {dev.addr} with PSM {psm} via L2CAP...')
-                    # s.connect((dev.addr, psm))
-                    # print('  >> Connected, attempting to send a string...')
-                    # s.send('Hello world!'.encode('utf8'))
+                    s = input(f'  >> Enter a string to write to characteristic: ').encode('utf8')
+                    c = characteristics[0]
+                    c.write(s, withResponse=False)
+                    print('  >> Wrote successfully!')
+                else:
+                    print('  >> Could not find our characteristic. :(')
                 peripheral.disconnect()
--- Scripts/gatt_constants.py
+++ Scripts/gatt_constants.py
@@ -1,3 +1,3 @@
 SERVICE_UUID = '59553ceb-2ffa-4018-8a6c-453a5292044d'        # Distributed Chat GATT service UUID
-CHARACTERISTIC_UUID = '440a594c-3cc2-494a-a08a-be8dd23549ff' # The GATT characteristic holding the L2CAP PSM
+CHARACTERISTIC_UUID = '440a594c-3cc2-494a-a08a-be8dd23549ff' # Message inbox GATT characteristic
                                                              # Is part of the mentioned GATT service
