969fcca51e5e3dcca257c7b5ccee747c9a538c01
fwcd <fwcdmail@gmail.com>
2021-01-23 16:15:03 +0100

    Add navigation bar to ProfileView

--- DistributedChatApp/DistributedChatApp/View/ProfileVIew.swift
+++ DistributedChatApp/DistributedChatApp/View/ProfileVIew.swift
@@ -14,15 +14,18 @@ struct ProfileView: View {
     //       chat user's profile views
     
     var body: some View {
-        VStack(alignment: .center, spacing: 40) {
-            Image(systemName: "person.circle")
-                .resizable()
-                .frame(width: 80, height: 80, alignment: .center)
-            TextField("Your nickname", text: $name)
-                .font(.title2)
-                .multilineTextAlignment(.center)
+        NavigationView {
+            VStack(alignment: .center, spacing: 40) {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 80, height: 80, alignment: .center)
+                TextField("Your nickname", text: $name)
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+            }
+            .padding(20)
+            .navigationBarTitle("Profile")
         }
-        .padding(20)
     }
 }
 
