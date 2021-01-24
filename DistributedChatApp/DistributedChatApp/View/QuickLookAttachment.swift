11308732936fd1008e744c0a6849527c04c0ef09
fwcd <fwcdmail@gmail.com>
2021-01-24 03:31:42 +0100

    Better logging in QuickLookAttachment

--- DistributedChatApp/DistributedChatApp/View/QuickLookAttachment.swift
+++ DistributedChatApp/DistributedChatApp/View/QuickLookAttachment.swift
@@ -7,7 +7,10 @@
 
 import DistributedChat
 import Foundation
+import Logging
 import QuickLook
+
+fileprivate let log = Logger(label: "QuickLookAttachment")
 
 class QuickLookAttachment: NSObject, QLPreviewItem {
     private let attachment: ChatAttachment
@@ -19,13 +22,14 @@ class QuickLookAttachment: NSObject, QLPreviewItem {
     init(attachment: ChatAttachment) throws {
         self.attachment = attachment
         let docDir = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        let tmpDir = docDir.appendingPathComponent("tmp")
-        try FileManager.default.createDirectory(at: tmpDir, withIntermediateDirectories: true)
-        tempURL = tmpDir.appendingPathComponent(attachment.name)
+        let attachmentsDir = docDir.appendingPathComponent("tmp")
+        try FileManager.default.createDirectory(at: attachmentsDir, withIntermediateDirectories: true)
+        tempURL = attachmentsDir.appendingPathComponent("\(UUID())-\(attachment.name)")
         try Data(contentsOf: attachment.url).write(to: tempURL) // might overwrite an old file with that attachment name
     }
     
     deinit {
+        log.debug("Cleaning up temporary file from attachment...")
         try? FileManager.default.removeItem(at: tempURL)
     }
 }
