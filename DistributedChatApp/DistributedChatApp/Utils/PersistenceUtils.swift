7901655f20c2cd171cb41e717c1ae415c8dc393f
fwcd <fwcdmail@gmail.com>
2021-01-24 19:01:48 +0100

    Rename persistence module to PersistenceUtils
    ...and extract a function for creating URLs to persistence files.

--- DistributedChatApp/DistributedChatApp/Utils/PersistenceUtils.swift
+++ DistributedChatApp/DistributedChatApp/Utils/PersistenceUtils.swift
@@ -1,5 +1,5 @@
 //
-//  Published+Persistence.swift
+//  PersistenceUtils.swift
 //  DistributedChatApp
 //
 //  Created by Fredrik on 1/24/21.
@@ -11,21 +11,26 @@ import Logging
 
 fileprivate let encoder = JSONEncoder()
 fileprivate let decoder = JSONDecoder()
-fileprivate let log = Logger(label: "Published+Persistence")
+fileprivate let log = Logger(label: "PersistenceUtils")
 
 private var subscriptions = [String: AnyCancellable]()
 
+func persistenceFileURL(path: String) -> URL {
+    let url = path
+        .split(separator: "/")
+        .reduce(try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)) {
+            $0.appendingPathComponent(String($1))
+        }
+    
+    log.debug("Creating directory for auto-persisted value")
+    try! FileManager.default.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true)
+    
+    return url
+}
+
 extension Published where Value: Codable {
     init(wrappedValue: Value, persistingTo path: String) {
-        let url = path
-            .split(separator: "/")
-            .reduce(try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)) {
-                $0.appendingPathComponent(String($1))
-            }
-        
-        log.debug("Creating directory for auto-persisted value")
-        try! FileManager.default.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true)
-        
+        let url = persistenceFileURL(path: path)
         let save = { (value: Value) in
             do {
                 try encoder.encode(value).write(to: url)
