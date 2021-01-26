22161d9d614f23bfcd4d76185825aeb5c1ce5892
fwcd <fwcdmail@gmail.com>
2021-01-26 03:02:33 +0100

    Implement successful connection in js test client

--- Scripts/js-test-client/index.js
+++ Scripts/js-test-client/index.js
@@ -8,79 +8,112 @@ const noble = require('noble'); // Central/GATT client
 const bleno = require('bleno'); // Peripheral/GATT server
 const readline = require('readline');
 const { v4: uuid4 } = require('uuid');
-const { rawListeners } = require('process');
 
-const serviceUUID = '59553ceb-2ffa-4018-8a6c-453a5292044d';
-const inboxCharacteristicUUID = '440a594c-3cc2-494a-a08a-be8dd23549ff';
-const userNameCharacteristicUUID = 'b2234f40-2c0b-401b-8145-c612b9a7bae1';
-const userIDCharacteristicUUID = '13a4d26e-0a75-4fde-9340-4974e3da3100';
+const serviceUUID = '59553ceb2ffa40188a6c453a5292044d';
+const inboxCharacteristicUUID = '440a594c3cc2494aa08abe8dd23549ff';
+const userNameCharacteristicUUID = 'b2234f402c0b401b8145c612b9a7bae1';
+const userIDCharacteristicUUID = '13a4d26e0a754fde93404974e3da3100';
 
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 noble.on('discover', peripheral => {
   console.log(`Found peripheral ${peripheral}`);
-  // peripheral.connect();
-  peripheral.discoverServices([serviceUUID], (err, services) => {
-    if (err) console.log(err);
+  noble.stopScanning();
+  peripheral.connect(err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
 
-    if (services) {
-      console.log(`Discovered DistributedChat service!`);
-      const service = services[0];
+    peripheral.discoverServices([serviceUUID], (err, services) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
 
-      service.discoverCharacteristics([
-        inboxCharacteristicUUID,
-        userNameCharacteristicUUID,
-        userIDCharacteristicUUID
-      ], (err, chars) => {
-        if (err) console.log(err);
+      if (services) {
+        console.log(`Discovered DistributedChat service!`);
+        const service = services[0];
 
-        const inboxChar = chars.find(c => c.uuid === inboxCharacteristicUUID);
-        const userNameChar = chars.find(c => c.uuid === userNameCharacteristicUUID);
-        const userIDChar = chars.find(c => c.uuid === userIDCharacteristicUUID);
-        
-        userNameChar.read((err, data) => {
-          if (err) console.log(err);
-          const userName = data.toString('utf-8');
+        service.discoverCharacteristics([
+          inboxCharacteristicUUID,
+          userNameCharacteristicUUID,
+          userIDCharacteristicUUID
+        ], (err, chars) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
 
-          userIDChar.read((err, data) => {
-            if (err) console.log(err);
-            const userID = data.toString('utf-8');
+          const inboxChar = chars.find(c => c.uuid === inboxCharacteristicUUID);
+          const userNameChar = chars.find(c => c.uuid === userNameCharacteristicUUID);
+          const userIDChar = chars.find(c => c.uuid === userIDCharacteristicUUID);
+          
+          userNameChar.read((err, data) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            const userName = data.toString('utf-8');
 
-            console.log(`Remote user has name ${userName} and ID ${userID}`);
-
-            rl.question('Please enter a name: ', myName => {
-              const myID = uuid4();
-
-              userNameChar.write(Buffer.from(myName, 'utf-8'));
-              userIDChar.write(Buffer.from(myID, 'utf-8'));
-
-              function chatREPL() {
-                rl.question('Please enter a message: ', content => {
-                  inboxChar.write(Buffer.from(JSON.stringify({
-                    visitedUsers: [],
-                    addedChatMessages: [
-                      {
-                        id: uuid4(),
-                        timestamp: Date.now() / 1000.0,
-                        author: {
-                          id: myID,
-                          name: myName
-                        },
-                        content: content
-                      }
-                    ]
-                  }), 'utf-8'));
-                });
+            userIDChar.read((err, data) => {
+              if (err) {
+                console.log(err);
+                return;
               }
+              const userID = data.toString('utf-8');
 
-              chatREPL();
+              console.log(`Remote user has name ${userName} and ID ${userID}`);
+
+              rl.question('Please enter a name: ', myName => {
+                const myID = uuid4();
+
+                function chatREPL() {
+                  rl.question('Please enter a message: ', content => {
+                    console.log(`Writing '${content}' to characteristic...`)
+                    // TODO: Noble does not seem to support long characteristics,
+                    //       see https://github.com/noble/noble/issues/13
+                    inboxChar.write(Buffer.from(JSON.stringify({
+                      visitedUsers: [],
+                      addedChatMessages: [
+                        {
+                          id: uuid4(),
+                          timestamp: Date.now() / 1000.0,
+                          author: {
+                            id: myID,
+                            name: myName
+                          },
+                          content: content
+                        }
+                      ]
+                    }), 'utf-8'), false, err => {
+                      if (err) {
+                        console.log(err);
+                        return;
+                      }
+                      chatREPL();
+                    });
+                  });
+                }
+
+                chatREPL();
+              });
             });
           });
         });
-      });
-    }
+      } else {
+        peripheral.disconnect();
+      }
+    });
   });
 });
 
-console.log('Scanning for devices...');
-noble.startScanning([serviceUUID]);
+noble.on('stateChange', state => {
+  if (state === 'poweredOn') {
+    console.log('Scanning for devices...');
+    noble.startScanning([serviceUUID], false, err => console.log(err));
+  } else {
+    console.log('Stopping scan...');
+    noble.stopScanning();
+  }
+});
