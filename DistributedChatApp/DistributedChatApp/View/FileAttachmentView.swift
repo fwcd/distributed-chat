3b8c1fcccbf51b9fd6523416108d5162c061cdbe
fwcd <fwcdmail@gmail.com>
2021-02-02 02:11:05 +0100

    Factor out QuickLookAttachmentView from FileAttachmentView

--- DistributedChatApp/DistributedChatApp/View/FileAttachmentView.swift
+++ DistributedChatApp/DistributedChatApp/View/FileAttachmentView.swift
@@ -11,36 +11,11 @@ import SwiftUI
 struct FileAttachmentView: View {
     let attachment: ChatAttachment
     
-    @State private var quickLookShown: Bool = false
-    @State private var shareSheetShown: Bool = false
-    
     var body: some View {
-        Button(action: { quickLookShown = true }) {
+        QuickLookAttachmentView(attachment: attachment) {
             HStack {
                 Image(systemName: "doc.fill")
                 Text(attachment.name)
-            }
-        }
-        .sheet(isPresented: $quickLookShown) {
-            VStack {
-                HStack {
-                    Button(action: { shareSheetShown = true }) {
-                        Image(systemName: "square.and.arrow.up")
-                            .font(.system(size: iconSize))
-                    }
-                    Spacer()
-                    Button(action: { quickLookShown = false }) {
-                        Text("Close")
-                            .foregroundColor(.primary)
-                    }
-                }
-                .padding(15)
-                if let item = try? QuickLookAttachment(attachment: attachment) {
-                    QuickLookView(item: item)
-                }
-            }
-            .sheet(isPresented: $shareSheetShown) {
-                ShareSheet(items: [attachment.url.smartResolved])
             }
         }
     }
---
+++ DistributedChatApp/DistributedChatApp/View/QuickLookAttachmentView.swift
@@ -0,0 +1,53 @@
+//
+//  ImageAttachmentView.swift
+//  DistributedChatApp
+//
+//  Created by Fredrik on 2/2/21.
+//
+
+import DistributedChat
+import SwiftUI
+
+struct QuickLookAttachmentView<Content>: View where Content: View {
+    let attachment: ChatAttachment
+    let content: () -> Content
+    
+    @State private var quickLookShown: Bool = false
+    @State private var shareSheetShown: Bool = false
+    
+    var body: some View {
+        Button(action: { quickLookShown = true }) {
+            content()
+        }
+        .sheet(isPresented: $quickLookShown) {
+            VStack {
+                HStack {
+                    Button(action: { shareSheetShown = true }) {
+                        Image(systemName: "square.and.arrow.up")
+                            .font(.system(size: iconSize))
+                    }
+                    Spacer()
+                    Button(action: { quickLookShown = false }) {
+                        Text("Close")
+                            .foregroundColor(.primary)
+                    }
+                }
+                .padding(15)
+                if let item = try? QuickLookAttachment(attachment: attachment) {
+                    QuickLookView(item: item)
+                }
+            }
+            .sheet(isPresented: $shareSheetShown) {
+                ShareSheet(items: [attachment.url.smartResolved])
+            }
+        }
+    }
+}
+
+struct QuickLookAttachmentView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuickLookAttachmentView(attachment: ChatAttachment(name: "test.txt", url: URL(string: "data:text/plain;base64,dGVzdDEyMwo=")!)) {
+            Text("Test")
+        }
+    }
+}
