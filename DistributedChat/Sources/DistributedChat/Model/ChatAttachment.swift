e500beaa931f1b18e5986be5501b07f65bdab7d3
fwcd <fwcdmail@gmail.com>
2021-02-05 22:13:01 +0100

    Embed Data directly in ChatAttachment

--- DistributedChat/Sources/DistributedChat/Model/ChatAttachment.swift
+++ DistributedChat/Sources/DistributedChat/Model/ChatAttachment.swift
@@ -4,14 +4,14 @@ public struct ChatAttachment: Codable, Identifiable, Hashable {
     public var id: UUID
     public var type: ChatAttachmentType
     public var name: String
-    public var url: URL // use data-URLs for embedding data directly
+    public var data: Data
     public var compression: Compression?
     
-    public init(id: UUID = UUID(), type: ChatAttachmentType = .file, name: String, url: URL, compression: Compression? = nil) {
+    public init(id: UUID = UUID(), type: ChatAttachmentType = .file, name: String, data: Data, compression: Compression? = nil) {
         self.id = id
         self.type = type
         self.name = name
-        self.url = url
+        self.data = data
         self.compression = compression
     }
     
