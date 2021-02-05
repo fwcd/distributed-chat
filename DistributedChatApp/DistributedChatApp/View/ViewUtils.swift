fd0e94097c324e654793ecba02a932591fa6d5ca
fwcd <fwcdmail@gmail.com>
2021-02-05 19:54:26 +0100

    Improve display name for single-user DM channels

--- DistributedChatApp/DistributedChatApp/View/ViewUtils.swift
+++ DistributedChatApp/DistributedChatApp/View/ViewUtils.swift
@@ -20,13 +20,21 @@ extension Optional where Wrapped == ChatChannel {
         case .room(let name)?:
             return name
         case .dm(let userIds)?:
-            return userIds
-                .filter { $0 != network.myId }
-                .map { (network.presences[$0] ?? network.offlinePresences[$0])?.user.displayName ?? $0.uuidString }
-                .joined(separator: ",")
+            if userIds.count == 1, let userId = userIds.first {
+                return name(of: userId, with: network)
+            } else {
+                return userIds
+                    .filter { $0 != network.myId }
+                    .map { name(of: $0, with: network) }
+                    .joined(separator: ",")
+            }
         case nil:
             return globalChannelName
         }
+    }
+    
+    private func name(of userId: UUID, with network: Network) -> String {
+        (network.presences[userId] ?? network.offlinePresences[userId])?.user.displayName ?? userId.uuidString
     }
     
     func displayName(with network: Network) -> String {
