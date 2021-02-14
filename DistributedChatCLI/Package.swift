b3103cdb806048293b550202d3fdd80a69d5affb
fwcd <fwcdmail@gmail.com>
2021-02-14 04:27:45 +0100

    Link to upstream PR

--- DistributedChatCLI/Package.swift
+++ DistributedChatCLI/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.2"),
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.1.2"),
         .package(url: "https://github.com/PureSwift/BluetoothLinux.git", .revision("e9dd7332b5ac92c09d3d9ff9244ac535afc5f493")),
-        // TODO: Use upstream again once PR is merged
+        // TODO: Use upstream again once https://github.com/PureSwift/GATT/issues/24 is merged
         // .package(url: "https://github.com/PureSwift/GATT.git", .revision("c3bbda8000e3b82486ca9a353725d1bfbc7701e8")),
         .package(name: "GATT", url: "https://github.com/fwcd/swift-gatt.git", .revision("9b742fa388d12af0959984f015e6e18c35b1dba1")),
         .package(name: "LineNoise", url: "https://github.com/andybest/linenoise-swift.git", .revision("78e9bc9b685ffd551af0f3ac4d6e2beb22afd33b")),
