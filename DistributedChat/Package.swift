0a2702f4380e9ab6f874e3510235bf02268a9045
fwcd <fwcdmail@gmail.com>
2023-02-20 04:58:52 +0100

    Bump a bunch of package dependencies

--- DistributedChat/Package.swift
+++ DistributedChat/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "DistributedChat",
     platforms: [
-        .iOS("13.0")
+        .macOS(.v10_15),
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -18,7 +19,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.3")
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.2.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- DistributedChatCLI/Package.swift
+++ DistributedChatCLI/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "DistributedChatCLI",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     products: [
         .executable(
             name: "DistributedChatCLI",
@@ -17,11 +21,9 @@ let package = Package(
         .package(path: "../DistributedChatSimulationProtocol"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.2"),
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.1.2"),
-        .package(url: "https://github.com/PureSwift/BluetoothLinux.git", .revision("e9dd7332b5ac92c09d3d9ff9244ac535afc5f493")),
-        // TODO: Use upstream again once https://github.com/PureSwift/GATT/issues/24 is merged
-        // .package(url: "https://github.com/PureSwift/GATT.git", .revision("c3bbda8000e3b82486ca9a353725d1bfbc7701e8")),
-        .package(name: "GATT", url: "https://github.com/fwcd/swift-gatt.git", .revision("9b742fa388d12af0959984f015e6e18c35b1dba1")),
-        .package(name: "LineNoise", url: "https://github.com/andybest/linenoise-swift.git", .revision("78e9bc9b685ffd551af0f3ac4d6e2beb22afd33b")),
+        .package(url: "https://github.com/PureSwift/BluetoothLinux.git", .revision("b086cec00bae14ff7e49508ae0328d37e1c7c5fc")),
+        .package(url: "https://github.com/PureSwift/GATT.git", .revision("0bf40a7e52cfed3d8740b0a1fea776f9f338aa9c")),
+        .package(name: "LineNoise", url: "https://github.com/andybest/linenoise-swift.git", .revision("cbf0a35c6e159e4fe6a03f76c8a17ef08e907b0e")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -33,7 +35,7 @@ let package = Package(
                 .product(name: "DistributedChatSimulationProtocol", package: "DistributedChatSimulationProtocol"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "WebSocketKit", package: "websocket-kit"),
-                .product(name: "BluetoothLinux", package: "BluetoothLinux"),
+                .product(name: "BluetoothLinux", package: "BluetoothLinux", condition: .when(platforms: [.linux])),
                 .product(name: "GATT", package: "GATT"),
                 .product(name: "LineNoise", package: "LineNoise"),
             ]
