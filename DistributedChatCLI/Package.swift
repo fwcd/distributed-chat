8471e39d53dc5cfe020d35a7cf5ad20e72809634
fwcd <fwcd@fwcd.dev>
2024-07-06 19:34:12 +0200

    Replace revision pins with branch versions
    The revisions are already locked in Package.resolved

--- DistributedChatCLI/Package.swift
+++ DistributedChatCLI/Package.swift
@@ -21,9 +21,9 @@ let package = Package(
         .package(path: "../DistributedChatSimulationProtocol"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.2"),
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.6.1"),
-        .package(url: "https://github.com/PureSwift/BluetoothLinux.git", .revision("b086cec00bae14ff7e49508ae0328d37e1c7c5fc")),
-        .package(url: "https://github.com/PureSwift/GATT.git", .revision("0bf40a7e52cfed3d8740b0a1fea776f9f338aa9c")),
-        .package(name: "LineNoise", url: "https://github.com/andybest/linenoise-swift.git", .revision("cbf0a35c6e159e4fe6a03f76c8a17ef08e907b0e")),
+        .package(url: "https://github.com/PureSwift/BluetoothLinux.git", .branch("master")),
+        .package(url: "https://github.com/PureSwift/GATT.git", .branch("master")),
+        .package(name: "LineNoise", url: "https://github.com/andybest/linenoise-swift.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
