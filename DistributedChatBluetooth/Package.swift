2603440a0075ceca3ef0e04e5c0d557fa97014a1
fwcd <fwcd@fwcd.dev>
2024-08-03 19:35:37 +0200

    Only add BluetoothLinux dependencies on Linux
    There seems to be some trouble with Xcode package resolution

--- DistributedChatBluetooth/Package.swift
+++ DistributedChatBluetooth/Package.swift
@@ -3,6 +3,26 @@
 
 import PackageDescription
 
+var packageDependencies: [Package.Dependency] = [
+    .package(path: "../DistributedChatKit"),
+]
+
+var targetDependencies: [Target.Dependency] = [
+    .product(name: "DistributedChatKit", package: "DistributedChatKit"),
+]
+
+#if os(Linux)
+packageDependencies += [
+    .package(url: "https://github.com/PureSwift/BluetoothLinux.git", .branch("master")),
+    .package(url: "https://github.com/PureSwift/GATT.git", .branch("master")),
+]
+
+targetDependencies += [
+    .product(name: "BluetoothLinux", package: "BluetoothLinux", condition: .when(platforms: [.linux])),
+    .product(name: "GATT", package: "GATT", condition: .when(platforms: [.linux])),
+]
+#endif
+
 let package = Package(
     name: "DistributedChatBluetooth",
     platforms: [
@@ -16,21 +36,13 @@ let package = Package(
             targets: ["DistributedChatBluetooth"]
         ),
     ],
-    dependencies: [
-        .package(path: "../DistributedChatKit"),
-        .package(url: "https://github.com/PureSwift/BluetoothLinux.git", .branch("master")),
-        .package(url: "https://github.com/PureSwift/GATT.git", .branch("master")),
-    ],
+    dependencies: packageDependencies,
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "DistributedChatBluetooth",
-            dependencies: [
-                .product(name: "DistributedChatKit", package: "DistributedChatKit"),
-                .product(name: "BluetoothLinux", package: "BluetoothLinux", condition: .when(platforms: [.linux])),
-                .product(name: "GATT", package: "GATT", condition: .when(platforms: [.linux])),
-            ]
+            dependencies: targetDependencies
         ),
         .testTarget(
             name: "DistributedChatBluetoothTests",
